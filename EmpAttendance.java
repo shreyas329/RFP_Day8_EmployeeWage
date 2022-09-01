4803a8439856a2be08ccef18ba831c3b3e854b5b
Shreyas Sharma P <57483308+shreyas329@users.noreply.github.com>
2022-09-01 21:01:34 +0530

    Checking Employee attendance.

---
+++ EmpAttendance.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.emp_wage;
+
+public class EmpAttendance {
+	public static void main (String[] args) {
+        int IS_FULL_TIME = 1;
+
+        int empCheck = (int)Math.floor(Math.random() * 10 ) % 2;
+
+        if(empCheck == IS_FULL_TIME) {
+            System.out.println("Employee is Present.");
+        }
+        else
+            System.out.println("Employee is Absent.");
+    }
+
+}
